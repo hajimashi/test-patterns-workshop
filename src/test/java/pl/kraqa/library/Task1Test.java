a99c967fdb11b0543cb74ff4092ca4ad8dc9888b
hajimashi <zolna.marcin@gmail.com>
2024-06-01 10:51:10 +0200

    Task1 solution

--- src/test/java/pl/kraqa/library/Task1Test.java
+++ src/test/java/pl/kraqa/library/Task1Test.java
@@ -1,5 +1,6 @@
 package pl.kraqa.library;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -12,5 +13,38 @@ import static org.junit.jupiter.api.Assertions.*;
  * - Create test checking that book is not available until a copy of a book is added
  */
 class Task1Test {
+    Author heniek;
+    Book krzyzacy;
+    Library library;
 
+    @BeforeEach
+    public void setup() {
+        heniek = new Author("Henryk", "Sienkiewicz");
+        krzyzacy = new Book(heniek, "Krzyżacy", 1897, "978-83-774-0824-7");
+        library = new Library();
+    }
+
+    @Test
+    public void addedBookMayBeFoundInTheLibrary() {
+        //when
+        library.addBook(krzyzacy);
+
+        //then
+        assertTrue(!library.findByTitle("Krzyżacy").isEmpty());
+    }
+
+    @Test
+    public void bookIsAvailableWhenCopyAdded() {
+        //given
+        library.addBook(krzyzacy);
+
+        //expect
+        assertFalse(library.isAvailable(krzyzacy));
+
+        //when
+        library.addCopy(krzyzacy.getISBN());
+
+        //then
+        assertTrue(library.isAvailable(krzyzacy));
+    }
 }
