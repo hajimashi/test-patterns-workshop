d97864647fe6e22bd323d7cd72824a2dfd6a929d
hajimashi <zolna.marcin@gmail.com>
2024-06-02 11:55:58 +0200

    Partial solution

--- src/test/java/pl/kraqa/web/KraQATest.java
+++ src/test/java/pl/kraqa/web/KraQATest.java
@@ -1,34 +1,13 @@
 package pl.kraqa.web;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chromium.ChromiumDriverLogLevel;
 
-
-import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class KraQATest {
-
-    WebDriver driver;
-
-    @BeforeEach
-    public void setupDriver() {
-        ChromeDriverService service = new ChromeDriverService.Builder()
-                .withLogLevel(ChromiumDriverLogLevel.INFO)
-                .withAppendLog(true)
-                .withLogOutput(System.out)
-                .build();
-        driver = new ChromeDriver(service);
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
-    }
+public class KraQATest extends WebDriverTest {
 
     @Test
     public void testHomePage() {
@@ -51,11 +30,6 @@ public class KraQATest {
         driver.findElement(By.name("submit")).click();
 
         assertTrue(driver.findElement(By.xpath("//*[contains(text(), 'Thanks for being with us')]")).isDisplayed());
-    }
-
-    @AfterEach
-    public void closeDriver() {
-        driver.quit();
     }
 
 }
---
+++ src/test/java/pl/kraqa/web/WebDriverTest.java
@@ -0,0 +1,31 @@
+package pl.kraqa.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chromium.ChromiumDriverLogLevel;
+
+import java.time.Duration;
+
+public class WebDriverTest {
+    WebDriver driver;
+
+    @BeforeEach
+    public void setupDriver() {
+        ChromeDriverService service = new ChromeDriverService.Builder()
+                .withLogLevel(ChromiumDriverLogLevel.INFO)
+                .withAppendLog(true)
+                .withLogOutput(System.out)
+                .build();
+        driver = new ChromeDriver(service);
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
+    }
+
+
+    @AfterEach
+    public void closeDriver() {
+        driver.quit();
+    }
+}
