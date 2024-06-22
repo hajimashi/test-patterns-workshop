d66f9432ccb3ab76cfa19702b293348018b80448
hajimashi <zolna.marcin@gmail.com>
2024-06-22 06:49:47 +0200

    page factory example

--- src/test/java/pl/kraqa/web/page/kraqa/ContactFormPage.java
+++ src/test/java/pl/kraqa/web/page/kraqa/ContactFormPage.java
@@ -1,47 +1,54 @@
 package pl.kraqa.web.page.kraqa;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class ContactFormPage extends BasePage<ContactFormPage> {
-    private By nameInput = By.name("name");
-    private By emailInput = By.name("email");
-    private By subjectInput = By.name("subject");
-    private By messageInput = By.name("message");
-    private By submitButton = By.name("submit");
-    private By confirmationMessage = By.xpath("//*[contains(text(), 'Thanks for being with us')]");
+    @FindBy(name = "name")
+    private WebElement nameInput;
+    @FindBy(name = "email")
+    private WebElement emailInput;
+    @FindBy(name = "subject")
+    private WebElement subjectInput;
+    @FindBy(name = "message")
+    private WebElement messageInput;
+    @FindBy(name = "submit")
+    private WebElement submitButton;
+    @FindBy(xpath = "//*[contains(text(), 'Thanks for being with us')]")
+    private WebElement confirmationMessage;
 
     public ContactFormPage(WebDriver webDriver) {
         super("https://kraqa.pl/kontakt/", webDriver);
     }
 
     public ContactFormPage fillName(String name) {
-        driver.findElement(nameInput).sendKeys(name);
+        nameInput.sendKeys(name);
         return this;
     }
 
     public ContactFormPage fillEmail(String email) {
-        driver.findElement(emailInput).clear();
-        driver.findElement(emailInput).sendKeys(email);
+        emailInput.clear();
+        emailInput.sendKeys(email);
         return this;
     }
 
     public ContactFormPage fillSubject(String subject) {
-        driver.findElement(subjectInput).sendKeys(subject);
+        subjectInput.sendKeys(subject);
         return this;
     }
 
     public ContactFormPage fillMessage(String message) {
-        driver.findElement(messageInput).sendKeys(message);
+        messageInput.sendKeys(message);
         return this;
     }
 
     public ContactFormPage submitForm() {
-        driver.findElement(submitButton).click();
+        submitButton.click();
         return this;
     }
 
     public boolean isConfirmationMessageDisplayed() {
-        return driver.findElement(confirmationMessage).isDisplayed();
+        return confirmationMessage.isDisplayed();
     }
 }
